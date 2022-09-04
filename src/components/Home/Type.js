659cb0e5a8a372513c6009fab9a62a98b0d90ad0
Shivam Sanju <shvmsanju@gmail.com>
2022-09-04 19:24:45 +0530

    changed text

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -5,7 +5,11 @@ function Type() {
   return (
     <Typewriter
       options={{
-        strings: ['Software Developer', 'Open Source Contributor', 'Traveller'],
+        strings: [
+          'Software Developer',
+          'Open Source Contributor',
+          'You know i am something of a scientist myself 😉',
+        ],
         autoStart: true,
         loop: true,
         deleteSpeed: 70,
